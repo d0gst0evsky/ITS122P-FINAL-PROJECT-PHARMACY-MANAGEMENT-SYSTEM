4c738ff3cadd1ec9955a2fefe7527cc8666152d8
d0gstoevsky <mercado.sebastianoliver@gmail.com>
2026-06-18 19:55:16 +0800

    Change database connection settings
    Updated database credentials for Clever Cloud deployment.

--- config.php
+++ config.php
@@ -2,11 +2,11 @@
 // config.php - database connection file
 // this file connects our pharmacy system to the database
 
-// database credentials
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = ""; // walang password sa localhost
-$db_name = "pharmacy_management";
+// database credentials (Updated for Clever Cloud)
+$db_host = "bjwp2fpsaulvdv9hejkp-mysql.services.clever-cloud.com";
+$db_user = "uk7tfecxmfqcfjb3";
+$db_pass = "ZGlaYFY13Pt8eX2LXM6t"; // <-- Pindutin mo yung orange lock sa Clever Cloud at i-paste rito ang password
+$db_name = "bjwp2fpsaulvdv9hejkp";
 
 // connect to the database
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
