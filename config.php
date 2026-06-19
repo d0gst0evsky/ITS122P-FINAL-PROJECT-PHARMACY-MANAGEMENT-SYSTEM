2578cd743f3bfa664181dbb05f29f765cd808e49
d0gstoevsky <mercado.sebastianoliver@gmail.com>
2026-06-20 00:21:41 +0800

    Update config.php
    added db

--- config.php
+++ config.php
@@ -1,17 +1,12 @@
 <?php
 // config.php - database connection file
-// this file connects our pharmacy system to the database
+$db_host = "bjwp2fpsaulvdv9hejkp-mysql.services.clever-cloud.com";
+$db_user = "uk7tfecxmfqcfjb3";
+$db_pass = "ZGlaYFY13Pt8eX2LXM6t"; // <-- Put the real password here!
+$db_name = "bjwp2fpsaulvdv9hejkp";
 
-// database credentials
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = ""; // walang password sa localhost
-$db_name = "pharmacy_management";
-
-// connect to the database
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-// check kung nag-connect
 if (!$conn) {
     die("Hindi makakonekta sa database: " . mysqli_connect_error());
 }
